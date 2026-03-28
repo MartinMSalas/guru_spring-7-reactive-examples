c526edc481f188d9760d73d88e71538049c7bd45
MartinMSalas <ingmartinmsalas@gmail.com>
2026-03-28 15:42:36 -0300

    test<PersonMonoTest>: testFindPersonByIdNotFound impl

--- src/test/java/com/sparta/spring_7_reactive_examples/repository/PersonRepositoryImplTest.java
+++ src/test/java/com/sparta/spring_7_reactive_examples/repository/PersonRepositoryImplTest.java
@@ -2,7 +2,11 @@ package com.sparta.spring_7_reactive_examples.repository;
 
 import com.sparta.spring_7_reactive_examples.domain.Person;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -72,8 +76,61 @@ class PersonRepositoryImplTest {
 
     }
 
+    @Test
+    void testFluxBlockFirst() {
+        Flux<Person> personFlux = personRepository.findAll();
+        Person person = personFlux.blockFirst();
+        IO.println(person);
+    }
 
-        @Test
+    @Test
+    void testFluxSuscribe(){
+        Flux<Person> personFlux = personRepository.findAll();
+        personFlux.map(Person::getFirstName).subscribe(System.out::println);
+
+    }
+
+    @Test
+    void testFluxToList(){
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Mono<List<Person>> listMono = personFlux.collectList();
+        listMono.subscribe(
+                list-> {
+                    list.forEach(System.out::println);
+                }
+        );
+    }
+
+    @Test
+    void testFilterOnName(){
+        Flux<Person> personFlux = personRepository.findAll();
+        Mono<List<Person>> listMono = personFlux.collectList();
+        listMono.subscribe(
+                list -> {
+                    list.stream().filter(person -> person.getFirstName().equals("Diostri")).forEach(System.out::println);
+                }
+        );
+    }
+
+    @Test
+    void anotherFilterOnName(){
+        personRepository.findAll().filter(person -> person.getFirstName().equals("Diostri"))
+                .subscribe(System.out::println);
+
+    }
+
+    @Test
+    void testFindPersonByIdNotFound(){
+        final Integer personId = 5;
+
+        Mono<Person> personMono = personRepository.findByPersonId(personId).single();
+
+
+        personMono.subscribe(System.out::println);
+    }
+
+    @Test
     void findAll() {
     }
 }
